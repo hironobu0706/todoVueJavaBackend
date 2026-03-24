471b35652881d26675d1692ed4b7d527361d2c04
obqd3614@yahoo.co.jp <obqd3614@yahoo.co.jp>
2026-03-24 12:27:40 +0900

    2回目コミット

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
-FROM maven:3.9-eclipse-temurin-17
-WORKDIR /app
-COPY target/*.jar app.jar
-CMD ["java","-jar","app.jar","--spring.profiles.active=docker"]
+# FROM maven:3.9-eclipse-temurin-17
+# WORKDIR /app
+# COPY target/*.jar app.jar
+# CMD ["java","-jar","app.jar","--spring.profiles.active=docker"]
 #CMD ["./mvnw", "spring-boot:run"]
+
+FROM eclipse-temurin:17
+WORKDIR /app
+COPY . .
+RUN chmod +x mvnw
+CMD ["./mvnw", "spring-boot:run"]
--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("api")
+@CrossOrigin(origins = "*") // このコントローラーの全メソッドを全許可
+//@CrossOrigin("http://localhost:3000") // https://todo-list-plus-front-hironobu0706-hironobu0706s-projects.vercel.app/
 public class SampleController {
 
-	@GetMapping("/api")
+	@GetMapping("/sample")
 	public String sample() {
-		System.out.println("aaaaaaaaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaaaaaaaa3");
 		return "";
 	}
 }
